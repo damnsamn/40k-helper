8eaf5c347a773a5a5fe870bf086a7b941c7d8091
Sam B <sam@humaan.com.au>
2022-04-25 00:04:11 +0800

    Updated commands for spaces and quotes

--- helpers.py
+++ helpers.py
@@ -40,3 +40,16 @@ def search_data(iterable, name=None, **data):
         print("Could not find model matching ", query)
     except Exception as e:
         print(format(e))
+
+class colors:
+    GREY = '\033[90m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    MAGENTA = '\033[95m'
+    CYAN = '\033[96m'
+
+    END = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
--- state.py
+++ state.py
@@ -18,3 +18,10 @@ def add_model(*args, **kwargs):
     if(model):
         army.append(model)
         print("Added model:", model.name)
+
+# def remove_model(ref):
+    # Ref can be an index or a name
+    # model = helpers.search_data(all_models, *args, **kwargs)
+    # if(model):
+    #     army.append(model)
+    #     print("Added model:", model.name)
